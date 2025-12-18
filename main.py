914d2bd6d18522a52effa42dda3425992306ae86
plcking777 <woutvandamme.wvd@gmail.com>
2025-12-18 21:31:46 +0100

    get N commits

---
+++ main.py
@@ -0,0 +1,31 @@
+import subprocess
+import sys
+
+N = 5
+
+
+if len(sys.argv) != 2:
+    raise ValueError("Expected a `repository path` argument")
+repo = sys.argv[1]
+
+
+# get N git commits
+git_log = subprocess.check_output(
+    [
+        "git",
+        "-C",
+        repo,
+        "log",
+        "--oneline",
+        "--all",
+        "--no-merges",
+        '--pretty=format:"%h"',
+    ]
+)
+
+
+git_log = git_log.decode("utf-8").replace('"', "").split("\n")
+
+
+git_log = git_log[0:N]
+print(git_log)
